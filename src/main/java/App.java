549181c6b864e6ffdbb7e22b1058e0b120f5c29c
Артем18061989-афк <artem18061989@mail.ru>
2025-10-06 16:27:33 +0400

    a

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,10 +3,22 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class App {
     public static void main(String[] args) {
-        ApplicationContext applicationContext =
-                new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+
+
+        HelloWorld hw1 = (HelloWorld) context.getBean("helloworld");
+        System.out.println(hw1.getMessage());
+
+
+        HelloWorld hw2 = (HelloWorld) context.getBean("helloworld");
+        System.out.println("Объекты HelloWorld совпадают: " + (hw1 == hw2));
+
+
+        Cat cat1 = (Cat) context.getBean("cat");
+        System.out.println(cat1.toString());
+
+
+        Cat cat2 = (Cat) context.getBean("cat");
+        System.out.println("Объекты Cat совпадают: " + (cat1 == cat2));
     }
 }
--- src/main/java/Cat.java
+++ src/main/java/Cat.java
@@ -1,4 +1,53 @@
-package PACKAGE_NAME;
-
 public class Cat {
+    private String name;
+    private int age;
+    private String breed;
+    private boolean isMale;
+
+    public Cat() {
+    }
+
+
+    public Cat(String name, int age, String breed, boolean isMale) {
+        this.name = name;
+        this.age = age;
+        this.breed = breed;
+        this.isMale = isMale;
+    }
+
+    public Cat(String murzik) {
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    public boolean isMale() {
+        return isMale;
+    }
+
+    public void setMale(boolean male) {
+        isMale = male;
+    }
 }
